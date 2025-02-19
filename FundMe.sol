b58d70c1222542d930e895c3cf22c8270a4637e1
nik-rawat <nr2192004@gmail.com>
2025-02-19 22:38:41 +0530

    completed all the smart contracts in fundMe

--- FundMe.sol
+++ FundMe.sol
@@ -1,34 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import { PriceConvertor } from "./PriceConvertor.sol";
+
+error notOwner();
 
 contract FundMe {
     using PriceConvertor for uint256;
 
     AggregatorV3Interface internal dataFeed;
 
-    uint256 public minimumUSD = 5e18;
+    uint256 public constant MINIMUN_USD = 5e18;
+
     address [] public funders;
     mapping (address funder => uint amountFunded) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor () {
-        dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        owner = msg.sender;
+        dataFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require( msg.value.getConversionRate() >= minimumUSD, "Didn't sent enough ETH");
+        require( msg.value.getConversionRate() >= MINIMUN_USD, "Didn't sent enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value.getConversionRate();
     }
 
     function withdraw() public onlyOwner {
 
-        // require(msg.sender == owner, "Must be owner");
+        // require(msg.sender == i_owner, "Must be owner");
         for(uint funderIndex=0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -48,8 +51,20 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Sender is not the owner!");
+        // require(msg.sender == i_owner, "Sender is not the owner!");
+        if (msg.sender == i_owner) {
+            revert notOwner();
+        }
         _;
     }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
 
 }
